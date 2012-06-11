1f99434387036291f7d0dc1486110d9b4ffe8001
Alex Chien <alex.chien@koocaa.com>
2012-06-11 18:03:14 +0800

    default sorting by news_date desc

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -30,6 +30,8 @@ RailsAdmin.config do |config|
       help "请勿直接贴入Word或者外部网页上复制的内容，请使用工具栏上的从‘粘贴为无格式文本’工具来帮助清理内容中的格式信息。标题请使用h1，段落使用p，首字无需手动缩进。"
     end
     list do
+      sort_by :news_date
+      sort_reverse true
       field :content do
         visible false
       end
