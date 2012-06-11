41e28fe82865ae60330d0f9650faf6e3b0d55565
Alex Chien <alex.chien@koocaa.com>
2012-06-11 18:03:26 +0800

    remove old paperclip extension

--- config/initializers/paperclip_extensions.rb
+++ config/initializers/paperclip_extensions.rb
@@ -1,3 +1,3 @@
-Paperclip.interpolates :id do |attachment, style|
-  ("%09d" % attachment.instance.id).scan(/d{3}/).join("/")
-end
+# Paperclip.interpolates :id do |attachment, style|
+#   ("%09d" % attachment.instance.id).scan(/d{3}/).join("/")
+# end
